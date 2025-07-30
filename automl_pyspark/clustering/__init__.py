2225d2d6ba8982071fcc42756cb8528ba74dece5
Sundar Krishnan <sundarstyles89@gmail.com>
2025-07-29 22:55:21 -0500

    Delete automl_pyspark directory

--- automl_pyspark/clustering/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Clustering Module
-
-This module contains classes and functions for clustering tasks in the AutoML system.
-"""
-
-from .automl_clusterer import AutoMLClusterer
-
-__all__ = ['AutoMLClusterer'] 
--- automl_pyspark/regression/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Regression Module
-
-This module contains classes and functions for regression tasks in the AutoML system.
-"""
-
-from .automl_regressor import AutoMLRegressor
-
-__all__ = ['AutoMLRegressor'] 
